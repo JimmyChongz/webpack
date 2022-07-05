9f026c3ba703cd7539e4b64de89d6873dd5ba02d
徐崇智 <xuchongzhi@xuchongzhideMacBook-Air.local>
2022-07-05 23:45:37 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js',
+  },
+//   devServer: {
+//     static: {
+//         directory: path.join(__dirname, '/')
+//     }
+//   },
+};
